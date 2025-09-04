d9e332c1cd0a927124aa2931cbd1591d2e764123
ViMinhThang <huynhminhthang246@gmail.com>
2025-09-04 15:04:38 +0700

    feat: place order

--- platform-ecom-backend/src/main/java/com/ecommerce/project/payload/OrderDTO.java
+++ platform-ecom-backend/src/main/java/com/ecommerce/project/payload/OrderDTO.java
@@ -1,8 +1,15 @@
 package com.ecommerce.project.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDTO {
 
     private Long orderId;
@@ -12,4 +19,5 @@ public class OrderDTO {
     private PaymentDTO paymentDTO;
     private Double totalAmount;
     private String orderStatus;
+    private Long addressId;
 }
