b47d36eee677097e3ee6796de67eb6b340c8bf7e
ViMinhThang <huynhminhthang246@gmail.com>
2025-09-20 13:32:41 +0700

    setup config server

--- platform-ecom-backend/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO categories ( category_name) VALUES ('shoes');
-INSERT INTO categories (category_name) VALUES ('clothing');
-INSERT INTO categories (category_name) VALUES ('accessories');
