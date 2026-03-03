5d8b2144b999e5c3f8140852bfb68ab29af2c195
ViMinhThang <huynhminhthang246@gmail.com>
2026-03-03 20:14:18 +0700

    coding recommendation service

--- init-multi-db.sql/03-chatbot-db.sql
+++
@@ -1,6 +0,0 @@
--- Create chatbot database
-CREATE DATABASE chatbot_db;
-
--- Connect to chatbot_db and enable pgvector extension
-\c chatbot_db;
-CREATE EXTENSION IF NOT EXISTS vector;
--- platform-ecom-backend/product/src/main/resources/db/migration/V1.1__enable_pg_trgm_and_index.sql
+++
@@ -1,2 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE INDEX IF NOT EXISTS idx_products_name_trgm ON products USING gin (name gin_trgm_ops);
